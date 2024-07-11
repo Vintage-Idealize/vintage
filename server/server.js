e15c90a5fa6deec42ce0c82e4845135be12d6902
Matheesha Wickramaarachchi <ashaninduwara2018@gmail.com>
2024-07-11 22:37:46 +0530

    small changes on server config

--- server/server.js
+++ server/server.js
@@ -3,5 +3,5 @@ const port=3001;
 const host='127.0.0.1';
 
 const server=app.listen(port,host,()=>{
-    console.log('Node server is listening to $(server.address().port)')
+    console.log(`Node server is listening to ${server.address().port}`)
 })
