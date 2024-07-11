7e0ba4c9396fd93afe2168a7b59595cfc0508bb9
Matheesha Wickramaarachchi <ashaninduwara2018@gmail.com>
2024-07-11 22:31:02 +0530

    basic server config done
    basic server config done

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(
+    express.urlencoded({
+        extended:true,
+    })
+);
+app.use(express.json());
+
+module.exports=app;
---
+++ server/server.js
@@ -0,0 +1,7 @@
+const app=require('./app');
+const port=3001;
+const host='127.0.0.1';
+
+const server=app.listen(port,host,()=>{
+    console.log('Node server is listening to $(server.address().port)')
+})
