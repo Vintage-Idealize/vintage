c2695a1a1a7a59dc564bf08289f692c2c68f2fcd
Matheesha Wickramaarachchi <ashaninduwara2018@gmail.com>
2024-07-12 22:00:46 +0530

    test

--- server/controller.js
+++ server/controller.js
@@ -1,22 +1,32 @@
-const users=[
-    {
-        id:1,
-        name:'John Doe',
-    },
-    {
-        id:2,
-        name:'Jane Doe',
-    }
-]
+const User = require('./model');
 
-const getUsers=(cb)=>{
-    cb(users);
+const getUsers = (re, res, next) => {
+    User.find()
+        .then(response => {
+            res.json({ response });
+        })
+        .catch(err => {
+            res.json({ message: err });
+        })
+}
+const addUser = (req, res, next) => {
+    const user = new User({
+        id: req.body.id,
+        name: req.body.name
+    });
+    user.save()
+        .then(response => {
+            res.json({ response });
+        })
+        .catch(err => {
+            res.json({ message: err });
+        })
 }
 
-const getUserById=(id,cb)=>{
-    const user=users.find(user=>user.id==id);
+const getUserById = (id, cb) => {
+    const user = users.find(user => user.id == id);
     cb(user);
 }
 
-exports.getUsers=getUsers;
-exports.getUserById=getUserById;
+exports.getUsers = getUsers;
+exports.getUserById = getUserById;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,28 @@
-const app=require('./app');
-const port=3001;
-const host='127.0.0.1';
-cosnt
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const app = require('./app');
+const port = 3001;
+const host = '127.0.0.1';
+const mongoose = require('mongoose');
 
-const server=app.listen(port,host,()=>{
+app.use(cors());
+app.use(express.json());
+
+const uri = 'mongodb+srv://adminteam:58ashates88$8@cluster0.ksxahua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const connect = async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log('Connected to the database');
+    } catch (err) {
+        console.log('Error connecting to the database');
+    }
+
+}
+
+connect();
+
+const server = app.listen(port, host, () => {
     console.log(`Node server is listening to ${server.address().port}`)
 })
