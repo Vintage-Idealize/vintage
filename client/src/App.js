857498cac1eeed73cdbcb43e9972b85f44d3a651
NethmaKarunathilaka <karunathilakanethma1@gmail.com>
2024-07-12 21:00:20 +0530

    Revert "add routings"
    This reverts commit 18272a90b75100217a0b85d80ad0e5f886f10935.

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,16 @@ import Dashboard from './components/dashboard/dashboard';
 import HomePage from './components/HomePage/HomePage';
 import ProductViewPage from './components/ProductViewPage/ProductViewPage';
 import CartPage from './components/CartPage/CartPage';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-    <Router>
-      <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/productViewPage" element={<ProductViewPage />} />
-        <Route path="/cartPage" element={<CartPage/>} />
-      </Routes>
-    </Router>  
+
+      {/* <Dashboard/> */}
+       <HomePage /> 
+      <ProductViewPage />
+      {/* <CartPage /> */}
     </div>
   );
 
