cad5981f0f90662c037ea0c80698a601e9338d68
NethmaKarunathilaka <karunathilakanethma1@gmail.com>
2024-07-12 21:05:21 +0530

    fix errors

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,24 @@ import Dashboard from './components/dashboard/dashboard';
 import HomePage from './components/HomePage/HomePage';
 import ProductViewPage from './components/ProductViewPage/ProductViewPage';
 import CartPage from './components/CartPage/CartPage';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-
-      {/* <Dashboard/> */}
-       <HomePage /> 
-      <ProductViewPage />
-      {/* <CartPage /> */}
+    <Router>
+      <Routes>
+        <Route path="" element={<HomePage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/productViewPage" element={<ProductViewPage />} />
+        <Route path="/cartPage" element={<CartPage/>} />
+      </Routes>
+    </Router>  
     </div>
   );
 
 }
 
 export default App;
+
